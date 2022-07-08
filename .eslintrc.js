75711c1e94084ca8f161fc76bc9181ca7ef39242
Kamil <kamil.esmund@gmail.com>
2022-07-08 21:34:58 +0200

    Config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  root: true,
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/label-has-associated-control': ['error', { required: { some: ['id'] } }],
+  },
+  settings: {
+    react: {
+      version: '17',
+    },
+  },
+};
